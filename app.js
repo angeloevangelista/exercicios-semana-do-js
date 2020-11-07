8b6d4c55bd4e517bfc2e5ede020d51f087b24e4a
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-11-06 23:20:11 -0300

    Adding exercises resolution

--- app.js
+++ app.js
@@ -5,7 +5,11 @@
     o novo array no console.
 */
 
-const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
+const randomNumbers = [10, 30, 15, 25, 50, 40, 5];
+
+const oddNumbers = randomNumbers.filter((number) => number % 2 !== 0);
+
+console.log(oddNumbers);
 
 /*
   02
@@ -13,7 +17,14 @@ const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
   - Exiba no console quantos números abaixo de 501 o array abaixo possui.
 */
 
-const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
+const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691];
+
+const countOfNumbersLessThan501 = crazyNumbers.reduce(
+  (accumulator, number) => (number < 501 ? accumulator + 1 : accumulator),
+  0
+);
+
+console.log(countOfNumbersLessThan501);
 
 /*
   03
@@ -22,7 +33,11 @@ const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
     exiba o novo array no console.
 */
 
-const numbers = [5, 7, 3]
+const numbers = [5, 7, 3];
+
+const squaredNumbers = numbers.map((number) => Math.pow(number, 2));
+
+console.log(squaredNumbers);
 
 /*
   04
@@ -33,17 +48,23 @@ const numbers = [5, 7, 3]
 */
 
 const tarantinoMovies = [
-  { name: 'Bastardos inglórios', release: 2009 },
-  { name: 'Pulp Fiction', release: 1994 },
-  { name: 'Kill Bill: Volume 2', release: 2004 },
-  { name: 'Quatro Quartos', release: 1995 },
-  { name: 'Sin City', release: 2005 },
-  { name: 'Era uma Vez em... Hollywood', release: 2019 },
-  { name: 'Django Livre', release: 2012 },
-  { name: 'Cães de Aluguel', release: 1992 },
-  { name: 'À Prova de Morte', release: 2007 },
-  { name: 'Kill Bill: Volume 1', release: 2003 }
-]
+  { name: "Bastardos inglórios", release: 2009 },
+  { name: "Pulp Fiction", release: 1994 },
+  { name: "Kill Bill: Volume 2", release: 2004 },
+  { name: "Quatro Quartos", release: 1995 },
+  { name: "Sin City", release: 2005 },
+  { name: "Era uma Vez em... Hollywood", release: 2019 },
+  { name: "Django Livre", release: 2012 },
+  { name: "Cães de Aluguel", release: 1992 },
+  { name: "À Prova de Morte", release: 2007 },
+  { name: "Kill Bill: Volume 1", release: 2003 },
+];
+
+const tarantinoMoviesReleasedBefore2000 = tarantinoMovies.filter(
+  (movie) => movie.release < 2000
+);
+
+console.table(tarantinoMoviesReleasedBefore2000);
 
 /*
   05
@@ -53,14 +74,18 @@ const tarantinoMovies = [
 */
 
 const tvShows = [
-  { name: 'Breaking Bad', releaseYear: 2008 },
-  { name: 'Mr. Robot', releaseYear: 2015 },
-  { name: 'True Detective', releaseYear: 2014 },
-  { name: 'Hannibal', releaseYear: 2013 },
-  { name: 'The Handmaid\'s Tale', releaseYear: 2017 },
-  { name: 'House M.D.', releaseYear: 2004 },
-  { name: 'Watchmen', releaseYear: 2019 }
-]
+  { name: "Breaking Bad", releaseYear: 2008 },
+  { name: "Mr. Robot", releaseYear: 2015 },
+  { name: "True Detective", releaseYear: 2014 },
+  { name: "Hannibal", releaseYear: 2013 },
+  { name: "The Handmaid's Tale", releaseYear: 2017 },
+  { name: "House M.D.", releaseYear: 2004 },
+  { name: "Watchmen", releaseYear: 2019 },
+];
+
+const tvShowsNames = tvShows.map((tvShow) => tvShow.name);
+
+console.log(tvShowsNames);
 
 /*
   06
@@ -74,15 +99,25 @@ const tvShows = [
 */
 
 const cart = [
-  { name: 'Dark Souls III', price: 95.03 },
-  { name: 'Shadow of the Tomb Raider', price: 101.19 },
-  { name: 'Sekiro: Shadows Die Twice', price: 179.99 },
-  { name: 'Resident Evil 2', price: 119.90 },
-  { name: 'Death Stranding', price: 149.99 }
-]
+  { name: "Dark Souls III", price: 95.03 },
+  { name: "Shadow of the Tomb Raider", price: 101.19 },
+  { name: "Sekiro: Shadows Die Twice", price: 179.99 },
+  { name: "Resident Evil 2", price: 119.9 },
+  { name: "Death Stranding", price: 149.99 },
+];
 
 /*
   - Nome 1
   - Nome 2
   - Nome 3
 */
+
+const itemPrefix = "- ";
+
+const gamesNamesString = cart.reduce(
+  (accumulatedNames, currentGame) =>
+    `${accumulatedNames}\n${itemPrefix}${currentGame.name}`,
+  ""
+);
+
+console.log(gamesNamesString);
